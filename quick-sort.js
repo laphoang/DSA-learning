33cd849cc03d2c02659f67d6a5fc5fb22211150a
laphoang <laphoang>
2025-03-19 23:23:41 +0700

    update quick sort to choose the middle element

--- quick-sort.js
+++ quick-sort.js
@@ -5,16 +5,22 @@ function quickSort(arr) {
     }
     // choose a pivot from arr
     // choose the last element
-    let pivot = arr[arr.length - 1];
+    //let pivot = arr.length - 1;
+    // choose the middle one
+    let pivot = Math.floor(arr.length / 2);
     // left array
     let left = [];
     // right array
     let right = [];
     // conquer: partition the array: rearrange the array around pivot
     // iterate through the array
-    for (let i = 0; i < arr.length - 1; i++) {    
+    for (let i = 0; i < arr.length; i++) {   
+        // if i === pivot, skip
+        if (i === pivot) {
+            continue;
+        } 
         // if arr[i] < pivot, add to left array
-        if (arr[i] < pivot) {
+        if (arr[i] < arr[pivot]) {
             left.push(arr[i]);
         }
         else {
@@ -22,7 +28,7 @@ function quickSort(arr) {
         }
     // return array with pivot and recursive call for left array and right array
 }
-    return [...quickSort(left), pivot, ...quickSort(right)];
+    return [...quickSort(left), arr[pivot], ...quickSort(right)];
 }
 
 test_arr = [5,1,7,2,1,2,6,13,82,-4];
